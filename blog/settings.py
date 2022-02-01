509584080f6b749b4f3ee7dcbf06ac3409feaf93
Nishanth-Gobi <nish.professional@gmail.com>
2022-02-01 10:24:36 +0530

    debug

--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2mrk%!m#n-o5pr*b=736tkokoarv^yqq#^6#ceojerd0syc6i%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+if sys.argv[1] == 'runserver':
+    DEBUG = True
 
 ALLOWED_HOSTS = []
 
